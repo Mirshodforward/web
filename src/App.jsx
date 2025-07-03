55515a7281e58c201351cdfa2b4148fd6d9d9374
Mirshod <forvardmf@gmail.com>
2025-07-03 22:48:29 +0500

    Yangi funksiya: mashina rangini tahrirlash

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>nima gapp</h1>
+        <h4>Qondaye</h4>
+        <button>Bosqani</button>
+        </div>
+    </>
+  )
+}
+
+export default App
